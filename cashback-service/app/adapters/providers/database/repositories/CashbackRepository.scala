b430f1a29d8057faf3ef6510279c327e56149254
spark <fabricio.jacob@outlook.com>
2023-06-16 22:48:14 -0300

    add

--- cashback-service/app/adapters/providers/database/repositories/CashbackRepository.scala
+++ cashback-service/app/adapters/providers/database/repositories/CashbackRepository.scala
@@ -1,10 +1,11 @@
 package adapters.providers.database.repositories
 
-import adapters.providers.database.data.{CashbackData, CashbackTable}
-import slick.dbio.{Effect, NoStream}
+import adapters.providers.database.data.CashbackData
+import slick.dbio.{DBIO, Effect, NoStream}
 import slick.jdbc.JdbcBackend.Database
 import slick.jdbc.SQLActionBuilder
 import slick.sql.FixedSqlAction
+import slick.jdbc.PostgresProfile.api._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import javax.inject.Singleton
@@ -16,7 +17,13 @@ class CashbackRepository {
 
   def save(action: FixedSqlAction[Int, NoStream, Effect.Write]) : Unit = {
     val db = Database.forConfig("slick.db")
-    val insertFuture = db.run(action)
+    val insertFuture = db.run {
+        for {
+          _ <- DBIO.seq(sqlu"START TRANSACTION")
+          result <- action
+          _ <- DBIO.seq(sqlu"COMMIT")
+        } yield result
+      }
 
     insertFuture.onComplete {
       case Success(_) => println("Dados do Cashback salvos com sucesso!")
