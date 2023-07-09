968b0bf61b110113a849cea2e9f99833cb7fbfdc
spark <fabricio.jacob@outlook.com>
2023-07-08 22:50:50 -0300

    add

--- cashback-service/app/entrypoint/controllers/CashbackController.scala
+++ cashback-service/app/entrypoint/controllers/CashbackController.scala
@@ -2,6 +2,7 @@ package entrypoint.controllers
 
 
 import entrypoint.controllers.converters.CashbackConverter
+import play.api.Configuration
 import play.api.libs.json.Json
 import play.api.mvc._
 import usecase.createcashback.{CreateCashbackUsecase, FindLastCashbackUsecase}
@@ -11,13 +12,14 @@ import javax.inject.{Inject, Singleton}
 @Singleton
 class CashbackController @Inject()( val controllerComponents: ControllerComponents,
                                     val cashbackCreate: CreateCashbackUsecase,
-                                    val findLastCashbackUsecase: FindLastCashbackUsecase) extends BaseController {
+                                    val findLastCashbackUsecase: FindLastCashbackUsecase,
+                                    private val config: Configuration) extends BaseController {
 
   def create = Action { request: Request[AnyContent] =>
     val json = request.body.asJson
     val result = json match {
       case Some(value) =>
-        val customer = CashbackConverter.converter(value)
+        val customer = CashbackConverter.converter(value, config.get[String]("perc.value"))
         cashbackCreate.execute(customer)
         Created
       case None => BadRequest
--- cashback-service/app/entrypoint/controllers/converters/CashbackConverter.scala
+++ cashback-service/app/entrypoint/controllers/converters/CashbackConverter.scala
@@ -9,16 +9,16 @@ import java.util.UUID
 
 object CashbackConverter {
 
-  def converter(json: JsValue) = {
+  def converter(json: JsValue, perc: String) = {
     val result = json.validate[CreateCashbackRequest]
     result match {
-      case JsSuccess(value, path) => transform(value)
+      case JsSuccess(value, path) => transform(value, BigDecimal(perc))
       case e: JsError => throw new RuntimeException("Error transform request. Details " + e.errors)
     }
   }
 
-  private def transform(request: CreateCashbackRequest) : Customer = {
-    val cashback = Cashback(UUID.randomUUID().toString, request.value, BigDecimal(0), LocalDateTime.now(), request.value)
+  private def transform(request: CreateCashbackRequest, perc: BigDecimal) : Customer = {
+    val cashback = Cashback(UUID.randomUUID().toString, request.value, BigDecimal(0), LocalDateTime.now(), request.value.*(perc))
     Customer(request.customer, List(cashback))
   }
 }
